aba1fce1e0957c33d55b4104aa91d614ff96c7b3
lyremote <liuyanoutsee@outlook.com>
2024-12-06 11:22:45 +0800

    java参数传递机制-值传递

--- basic-practice/src/main/java/com/lyflexi/basicpractice/valuePropagation/Main.java
+++ basic-practice/src/main/java/com/lyflexi/basicpractice/valuePropagation/Main.java
@@ -6,23 +6,57 @@ package com.lyflexi.basicpractice.valuePropagation;
  */
 public class Main {
     public static void main(String[] args) {
+        System.out.println("----------------testBasicValue");
         int number = 10;
-        System.out.println("Before calling modifyValue: " + number);
-        modifyValue(number);
-        System.out.println("After calling modifyValue: " + number);
+        System.out.println("Before calling testBasicValue: " + number);
+        testBasicValue(number);
+        System.out.println("After calling testBasicValue: " + number);
 
-        StringBuilder stringBuilder = new StringBuilder("Hello");
-        System.out.println("Before calling modifyReference: " + stringBuilder);
-        modifyReference(stringBuilder);
-        System.out.println("After calling modifyReference: " + stringBuilder);
+        System.out.println("----------------testWrapperValue");
+        Integer numberInteger = 10;
+        System.out.println("Before calling testWrapperValue: " + number);
+        testWrapperValue(numberInteger);
+        System.out.println("After calling testWrapperValue: " + number);
+
+        System.out.println("----------------testReference");
+        ObjectReference reference = new ObjectReference(10);
+        System.out.println("Before calling testReference: " + reference);
+        testReference(reference);
+        System.out.println("After calling testReference: " + reference);
     }
 
-    public static void modifyValue(int num) {
+    /**
+     * 值传递：方法修改不影响传入值
+     *
+     * 改完值还是10
+     * @param num
+     */
+    public static void testBasicValue(int num) {
         num = 20;
     }
 
-    public static void modifyReference(StringBuilder strBuilder) {
-        strBuilder.append(" World");
+    /**
+     * 值传递：方法修改不影响传入值
+     *
+     * 改完值还是10
+     * @param num
+     */
+    public static void testWrapperValue(Integer num) {
+        num = 20;
+    }
+
+    /**
+     * 值传递：引用副本也是值
+     *
+     * 传入10，相当于传入了引用副本为10，内部改为20 ，则外部也改为20
+     *
+     * 但最终新创建的ObjectReference不属于同一引用，因此30不影响原来的引用
+     * @param reference
+     */
+    public static void testReference(ObjectReference reference) {
+        reference.setValue(20);
+        reference = new ObjectReference();
+        reference.setValue(30);
     }
 }
 
---
+++ basic-practice/src/main/java/com/lyflexi/basicpractice/valuePropagation/ObjectReference.java
@@ -0,0 +1,18 @@
+package com.lyflexi.basicpractice.valuePropagation;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Description:
+ * @Author: lyflexi
+ * @project: basic-jdk-practice
+ * @Date: 2024/12/6 11:04
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ObjectReference {
+    private Integer value;
+}
